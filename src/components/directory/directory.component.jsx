60445e1ac5327fff20f862f4e081a9f489f2d35a
Ana Zitzow <arzitzow@gmail.com>
2022-09-22 15:31:25 -0700

    categories and home page done

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import './directory.styles.scss';
+import { CategoryItem } from "../category-item/category-item.component";
+
+export const Directory = ({ categories }) => {
+
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}
