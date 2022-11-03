4742de8f974abf8a6f9da41d894413a17ba661b9
Ana Zitzow <arzitzow@gmail.com>
2022-11-03 11:43:45 -0700

    created the navigation bar with links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/routes/home/home.component.jsx";
+import { Navigation } from "./components/routes/navigation/navigation.component.jsx";
 
 export const App = () => {
   return (
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigation />} >
+          <Route index element={<Home />} />
+          <Route path="shop" element={<h1>Shop</h1>} />
+        </Route>
       </Routes>
     );
 };
---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,31 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import { ReactComponent as CrownLogo } from '../../../assets/crown.svg';
+import "./navigation.styles.scss";
+
+export const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrownLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/contact">
+            CONTACT
+          </Link>
+          <Link className="nav-link" to="/signin">
+            SIGN IN
+          </Link>
+          <Link className="nav-link" to="/cart">
+            CART
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+}
