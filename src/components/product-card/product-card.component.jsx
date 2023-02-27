d3661746aade2503350dfa4909080276ddb6dcd8
Ana Zitzow <arzitzow@gmail.com>
2023-02-27 10:55:09 -0800

    Cart dropdown and product card created and currently working

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,15 @@
 import './product-card.styles.scss'
-
+import {useContext} from 'react';
 import {Button} from '../button/button.component'
+
+import {CartContext} from '../../contexts/cart.context.jsx';
 
 export const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
@@ -12,7 +18,7 @@ export const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{`$${price}`}</span>
       </div>
-      <Button buttonType="inverted">ADD TO CART</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>ADD TO CART</Button>
     </div>
   );
 }
